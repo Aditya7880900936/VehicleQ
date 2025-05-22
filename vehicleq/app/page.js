0c8f9f9963f5c45f9bde9c5d62482dd039e2265f
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-05-23 02:32:37 +0530

    changes mdae in layout.js like adding font header and footer

--- vehicleq/app/page.js
+++ vehicleq/app/page.js
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       Subscribe to Roadside Coder 
+      <Button variant="destructive">
+        Hello
+      </Button>
     </div>
   );
 }
---
+++ vehicleq/components/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <header className="bg-blue-50 py-4">
+            <div className="container mx-auto px-4">
+            <h1 className="text-3xl font-bold text-center text-gray-800">
+                VehicleQ
+            </h1>
+            <p className="text-center text-gray-600">
+                Find your next vehicle
+            </p>
+            </div>
+        </header>
+    </div>
+  )
+}
+
+export default Header
