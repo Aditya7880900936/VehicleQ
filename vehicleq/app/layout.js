508f9ec8f8fbe315a6961df58d95359f3d1a4aaa
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-05-27 16:49:00 +0530

    Making the Ui of header section and implemnted the clerk auth

--- vehicleq/app/layout.js
+++ vehicleq/app/layout.js
@@ -1,10 +1,11 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
-})
+});
 
 export const metadata = {
   title: "VehicleQ",
@@ -13,19 +14,19 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        <Header/>
-        <main className="min-h-screen">{children}</main>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
 
-        <footer className="bg-blue-50 py-12 ">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made with 💗 Roadside Coder</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <footer className="bg-blue-50 py-12 ">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 Roadside Coder</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- vehicleq/components/components/Header.jsx
+++ vehicleq/components/components/Header.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import { SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+import { CarFront, Heart } from "lucide-react";
 
-const Header = () => {
+const Header = async ({ isAdminPage = false }) => {
+  const isAdmin = false;
+
   return (
-    <div>
-        <header className="bg-blue-50 py-4">
-            <div className="container mx-auto px-4">
-            <h1 className="text-3xl font-bold text-center text-gray-800">
-                VehicleQ
-            </h1>
-            <p className="text-center text-gray-600">
-                Find your next vehicle
-            </p>
-            </div>
-        </header>
-    </div>
-  )
-}
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdminPage ? "/admin" : "/"}>
+          <Image
+            src={"/logo1.png"}
+            alt="vehicleq logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">Admin</span>
+          )}
+        </Link>
 
-export default Header
+        <div className="flex items-center space-x-4">
+          <SignedIn>
+            <Link href="/saved-cars">
+              <Button>
+                <Heart size={18} />
+                <span className="hidden md:inline">Saved Cars</span>
+              </Button>
+            </Link>
+            <Link href="/my-reservations">
+              <Button variant="outline">
+                <CarFront size={18} />
+                <span className="hidden md:inline">My Reservations</span>
+              </Button>
+            </Link>
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
